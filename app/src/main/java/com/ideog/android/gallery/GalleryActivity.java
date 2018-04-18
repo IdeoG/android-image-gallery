f3f3a75b677154d359263f190a63c22282d0aeb5
ideog <ideogaib@gmail.com>
2018-04-19 01:04:22 +0300

    upd

--- app/src/main/java/com/ideog/android/gallery/GalleryActivity.java
+++ app/src/main/java/com/ideog/android/gallery/GalleryActivity.java
@@ -1,41 +1,23 @@
 package com.ideog.android.gallery;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -91,7 +73,8 @@ public class GalleryActivity extends AppCompatActivity {
                 .appendQueryParameter("sort", "interestingness-asc")
                 .appendQueryParameter("format", "json")
                 .appendQueryParameter("nojsoncallback", "1")
-                .appendQueryParameter("extras", "url_s")
+                .appendQueryParameter("extras", "url_m")
+                .appendQueryParameter("per_page", "50")
                 .build().toString();
         Log.i(TAG, "findImages: url request = " + url);
         OkHttpClient client = new OkHttpClient();
@@ -111,10 +94,10 @@ public class GalleryActivity extends AppCompatActivity {
                     JSONObject raw = new JSONObject(resp);
                     JSONObject photos = raw.getJSONObject("photos");
                     JSONArray photo = photos.getJSONArray("photo");
-                    int len = (photo.length() > 20) ? 20 : photo.length();
+                    int len = photo.length();
                     imageUrls.clear();
                     for (int i=0; i < len; i++) {
-                        String url = photo.getJSONObject(i).getString("url_s");
+                        String url = photo.getJSONObject(i).getString("url_m");
                         imageUrls.add(url);
                     }
 
