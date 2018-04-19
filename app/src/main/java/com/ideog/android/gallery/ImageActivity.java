0ff25bcb52cef4be5cb1a87ab34364a68310b593
ideog <ideogaib@gmail.com>
2018-04-19 12:25:48 +0300

    merge

--- app/src/main/java/com/ideog/android/gallery/ImageActivity.java
+++ app/src/main/java/com/ideog/android/gallery/ImageActivity.java
@@ -1,8 +1,8 @@
 package com.ideog.android.gallery;
 
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.ImageView;
